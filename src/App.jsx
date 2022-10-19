00cbf7c814ee429edc02db09ae14799549776e06
carreuky <carreuky@gmail.com>
2022-10-19 15:21:17 +0300

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import Signup from './components/Signup'
-import Login from './components/Login'
-import Questions from './components/Questions/Questions'
-import  {BrowserRouter, Route, Routes} from "react-router-dom"
-import About from './components/About'
-import Home from './components/Home'
-
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import Questions from "./components/Questions/Questions";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       {/* <Signup/> */}
       <BrowserRouter>
-      <Routes>
-      <Route exact="true" path="/" element={<Home/>}/>
+        <Routes>
+          <Route exact="true" path="/" element={<Home />} />
 
-        <Route exact="true" path="/login" element={<Login/>}/>
-        <Route exact="true" path="/signup" element={<Signup/>}/>
-        <Route exact="true" path="/questions" element={<Questions/>}/>
-        <Route exact="true" path="/about" element={<About/>}/>
-      </Routes>
+          <Route exact="true" path="/login" element={<Login />} />
+          <Route exact="true" path="/signup" element={<Signup />} />
+          <Route exact="true" path="/questions" element={<Questions />} />
+          <Route exact="true" path="/about" element={<About />} />
+        </Routes>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,110 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer class=" bg-dark row row-cols-5 py-5  border-top text-white">
+      <div class="col-lg-3">
+        <a
+          href="/"
+          class="d-flex align-items-center mb-3 link-dark text-decoration-none"
+        ></a>
+        <h1 class="text-muted ">
+          <i style={{ color: "#f48d4f" }} class="m-5 bi bi-stack-overflow"></i>
+        </h1>
+      </div>
+
+      <div class="col-lg-3">
+        <h5>Section</h5>
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">
+              Home
+            </a>
+          </li>
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">
+              Features
+            </a>
+          </li>
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">
+              Pricing
+            </a>
+          </li>
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">
+              FAQs
+            </a>
+          </li>
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">
+              About
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="col-lg-3">
+        <h5>Section</h5>
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">
+              Home
+            </a>
+          </li>
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">
+              Features
+            </a>
+          </li>
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">
+              Pricing
+            </a>
+          </li>
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">
+              FAQs
+            </a>
+          </li>
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">
+              About
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="col-lg-3">
+        <h5>Section</h5>
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">
+              Home
+            </a>
+          </li>
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">
+              Features
+            </a>
+          </li>
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">
+              Pricing
+            </a>
+          </li>
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">
+              FAQs
+            </a>
+          </li>
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">
+              About
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
