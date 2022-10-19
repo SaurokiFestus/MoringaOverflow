79de2688ac959133b9c0d68cbc9c65ff358085bc
LeilaniMigisi <leilani.migisi@student.moringaschool.com>
2022-10-19 11:46:07 +0300

    Added about component and its route

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -3,8 +3,6 @@ import React from 'react';
 function Questions(){
     return (
     <div className = "heading">
-        <h1>All questions</h1>
-        <h2> Why isnt it moving</h2>
     </div>
 )
 
