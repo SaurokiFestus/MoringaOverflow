57bfd514f7bf95113932aa8fc8d06f1481816aa6
Donald Brooks Arum <brooksd079@gmail.com>
2022-10-19 12:40:51 +0300

    sidebar template made

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Tags = () => {
   return (
-    <div>Tags</div>
+    <div>
+        <h3>Tags</h3>
+        <p>A tag is a keyword or label that categorizes your question with other, similar questions. Using the right tags makes it easier for others to find and answer your question.</p>
+        <a href="#">Show all tag synonyms</a>
+        <div></div>
+    </div>
   )
 }
 
