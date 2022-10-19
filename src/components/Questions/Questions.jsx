540daab5aa0f5a7923ae9299edb036440d0e3666
LeilaniMigisi <leilani.migisi@student.moringaschool.com>
2022-10-19 11:46:02 +0300

    Added about component and its route

---
+++ src/components/Questions/Questions.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Questions(){
+    return (
+    <div className = "heading">
+        <h1>All questions</h1>
+        <h2> Why isnt it moving</h2>
+    </div>
+)
+
+}
+export default Questions
