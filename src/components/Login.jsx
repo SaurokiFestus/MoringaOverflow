127ddc82c5534f12db754a63cc33b9040b9dbe7b
Donald Brooks Arum <brooksd079@gmail.com>
2022-10-23 15:29:25 +0300

    login form parameter bug fixed

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate } from "react-router-dom"
 
-function Login() {
+function Login({ onLogin, user }) {
   const history = useHistory()
     
   const[username, setUsername] = useState("")
