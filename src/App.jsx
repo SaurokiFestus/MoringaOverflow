04cec995356c56798aa9450568762d86fd560171
LeilaniMigisi <leilani.migisi@student.moringaschool.com>
2022-10-24 09:05:25 +0300

    Removed tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,8 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import Signup from './components/Signup'
 import Login from './components/Login'
-import Questions from './components/Questions'
 import  {BrowserRouter, Route, Routes} from "react-router-dom"
-import About from './components/About'
+// import About from './components/About'
 import Home from './components/Home'
 
 
@@ -20,8 +19,8 @@ function App() {
 
         <Route exact="true" path="/login" element={<Login/>}/>
         <Route exact="true" path="/signup" element={<Signup/>}/>
-        <Route exact="true" path="/questions" element={<Questions/>}/>
-        <Route exact="true" path="/about" element={<About/>}/>
+        {/* <Route exact="true" path="/allquestions" element={<Allquestions/>}/> */}
+        {/* <Route exact="true" path="/about" element={<About/>}/> */}
       </Routes>
       </BrowserRouter>
     </div>
