8b5b08916aba665cc7412a8dcc3c9abe5b6f17f5
LeilaniMigisi <leilani.migisi@student.moringaschool.com>
2022-10-24 12:55:12 +0300

    Added questions

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import './App.css'
 import Signup from './components/Signup'
 import Login from './components/Login'
 import  {BrowserRouter, Route, Routes} from "react-router-dom"
-// import About from './components/About'
 import Home from './components/Home'
 import SideBar from './components/Sidebar/Sidebar'
-
+import About from './components/About'
 import Navbar from './components/Navbar'
+import Questions from './components/Questions'
 
 
 function App() {
@@ -22,8 +22,9 @@ function App() {
 
         <Route exact="true" path="/login" element={<Login/>}/>
         <Route exact="true" path="/signup" element={<Signup/>}/>
-        {/* <Route exact="true" path="/questions" element={<Questions/>}/> */}
-        {/* <Route exact="true" path="/about" element={<About/>}/> */}
+        <Route exact="true" path="/signup" element={<Signup/>}/>
+        <Route exact="true" path="/questions" element={<Questions/>}/>
+        <Route exact="true" path="/about" element={<About/>}/>
         <Route exact="true" path="/sidebar" element={<SideBar/>}/>
       </Routes>
       </BrowserRouter>
