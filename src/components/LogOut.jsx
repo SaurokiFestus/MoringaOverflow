e61c5cb69a14fee6e04fc0680a0567eeded84e4d
Donald Brooks Arum <brooksd079@gmail.com>
2022-10-26 04:35:47 +0300

    Log out component mostly done

--- src/components/LogOut.jsx
+++ src/components/LogOut.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const LogOut = () => {
-  return (
-    <div>LogOut</div>
-  )
+function LogOut({ user, setUser }) {
+    let navigate = useNavigate()
+
+    // console.log(user.name)
+
+    function handleLogout (e) {
+
+        fetch(`/logout`, {
+            method: "DELETE"
+        })
+            .then((res)=>{
+                if (res.ok){
+                    setUser(null)
+                }
+            })
+        navigate('/signup')
+    
+    }
+
+    return (
+        <div>
+            sign 
+        </div>
+    )
 }
 
-export default LogOut
+export default LogOut;
