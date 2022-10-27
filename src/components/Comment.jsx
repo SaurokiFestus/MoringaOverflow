0b45342505e9d6bb49fb99f2b32951360592e905
carreuky <carreuky@gmail.com>
2022-10-27 09:34:19 +0300

    resolve refresh error on submit comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,37 +1,31 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 export default function Comment({ x, answer }) {
   const [newC, setNewC] = useState();
-  const [comments,setComments]= useState(x)
+  const [comments,setComments] = useState(x);
   const [showComments, setShowComments] = useState(false);
 
   function updateList(updatedItem) {
-    const updatedItems = comments.map((comment) => {
-      if (comment.id === updatedItem.id) {
-        return updatedItem;
-      } else {
-        return comment;
-      }
-    })
-    console.log(updatedItems)
-  };
+    console.log(comments)
+      setComments([...comments, updatedItem]);
+    console.log(comments);
+  }
 
-  function Submit(){
+  function Submit(e) {
+    e.preventDefault();
     console.log("vipi");
-    // e.preventDefault()
-
     fetch("http://127.0.0.1:3000/comments", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         body: newC,
         user_id: 2,
-        answer_id: answer.id
+        answer_id: answer.id,
       }),
     })
       .then((response) => response.json())
       .then((data) => updateList(data));
-  };
+  }
   const body = comments?.map((comment) => {
     return (
       <li className="pb-1" key={comment.id}>
@@ -41,30 +35,9 @@ export default function Comment({ x, answer }) {
   });
   return (
     <div>
-      <form>
-        <div className="row">
-          <div className="col-1"></div>
-          <div class="d-flex col-10">
-            <form>
-              <input
-                onChange={(e) => setNewC(e.target.value)}
-                type="text"
-                class="form-control"
-                id="inputPassword2"
-                placeholder="Leave a comment
-                    "
-              />
-            </form>
-            <input onClick={() => Submit()} type="submit" class="btn" />
-          </div>
-        </div>
-      </form>
-
-      <div className="row">
-        <div className="col-1"></div>
-        <div className="col-10">
-          <hr className=""></hr>
-        </div>
+      <div className="col-1"></div>
+      <div className="col-10">
+        <hr className=""></hr>
       </div>
 
       <button
@@ -75,13 +48,28 @@ export default function Comment({ x, answer }) {
       >
         Comments
       </button>
+
       {showComments ? (
         <div className="row p-0">
           <div className="col-1"></div>
           <div className="col-10">
             <ul className="list-unstyled">{body}</ul>
             <hr className=""></hr>
+            <div class="col-10 d-flex">
+            <form>
+              <input
+                onChange={(e) => setNewC(e.target.value)}
+                type="text"
+                class="form-control"
+                id="inputPassword2"
+                placeholder="Leave a comment
+                    "
+              />
+            </form>
+            <input onClick={(e) => Submit(e)} type="submit" class="btn" />
           </div>
+          </div>
+          
         </div>
       ) : (
         ""
