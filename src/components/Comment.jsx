0ca9d54d7e33b77df5404d6e11283a76150ad54c
LeilaniMigisi <leilani.migisi@student.moringaschool.com>
2022-10-31 10:05:01 +0300

    Completed accordion

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -55,19 +55,13 @@ export default function Comment({ x, answer }) {
       <div className="col" >
         <hr className=""></hr>
       </div>
-
-      <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
-
-      
-        </div>
-        <div class="accordion" id="accordionPanelsStayOpenExample">
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne" onClick={() => setShowComments(!showComments)}>
-        Comments
-      </button>
-    </h2>
-    
+      <div class="accordion" id="accordionPanelsStayOpenExample">
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+         <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne" onClick={() => setShowComments(!showComments)}>
+          Comments
+          </button>
+         </h2>
   </div>
 </div>
       {showComments ? (
