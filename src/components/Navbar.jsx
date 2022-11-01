e3cb44e7450cc7a8ccd374e0ef56d70ce4c13c50
SaurokiFestus <sauroki.festus@student.moringaschool.com>
2022-11-01 10:47:37 +0300

    styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 // import Questions from "./Questions"
 
 function Navbar({user, setWordEntered}) {
-  const flowColor = { color: "#f48d4f" };
+  const flowColor = { color: "#f48d4f", fontSize: "30px"};
 
   return (
     <div>
